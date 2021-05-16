39626aec456cad7ea217fd515be7f2eccc94b315
Fernando Penha <FZPenha@gmail.com>
2021-05-16 16:22:47 -0300

    revert

--- frontend/src/pages/FaleConosco.vue
+++ frontend/src/pages/FaleConosco.vue
@@ -37,6 +37,49 @@
             </section>
             
             <section>
+                <div>
+                    <head>
+                        <title>Fale Conosco</title>
+                    </head>
+                    <body>
+                        <section>
+                            <div class="container_sobre">
+                                <div class="sobre_sec1">
+                                <br/>
+                                    <div class="linha_sec1"></div>
+                                    <div class="sobre_sec1_titulo">Entre em contato conosco</div>
+                                    <div style="width: 70vw; display: grid; grid-template-columns: 40% 40%;">
+                                    <div class="FaleConoscoComponent">
+                                        <p>Nome:</p>
+                                        <input id="FaleConoscoNome" type="text" placeholder="ex: Fernando"/>
+                                    </div>
+                                    <div class="FaleConoscoComponent">
+                                        <p>Sobrenome:</p>
+                                        <input id="FaleConoscoSobrenome" type="text" placeholder="ex: Zagatto Penha"/>
+                                    </div>
+                                    <div class="FaleConoscoComponent">
+                                        <p>Instituição / Empresa:</p>
+                                        <input id="FaleConoscoInstituicao" type="text" placeholder="ex: IEEE CS - Guaratinguetá"/>
+                                    </div>
+                                    <div class="FaleConoscoComponent">
+                                        <p>E-mail:</p>
+                                        <input id="FaleConoscoEmail" type="text" placeholder="ex: csguara@gmail.com"/>
+                                    </div>
+                                    <div class="FaleConoscoComponent" style="grid-column: span 2;">
+                                        <p>Descrição do Projeto e Instituição:</p>
+                                        <textArea rows="4" id="FaleConoscoDescricaoProjeto" type="text" placeholder="ex: Olá, meu nome é C3PO e eu gostaria de conversar sobre uma parceria/projeto/*assunto* com o propósito de....."/>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </section>
+                        
+                        <section>
+                        </section>
+
+
+                    </body>
+                </div>
             </section>
 
 
