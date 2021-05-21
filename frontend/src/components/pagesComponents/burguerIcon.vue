8909de5baf91fd6248549a0cc4f1a2a2a83f2dc9
Luis Rossi <lcrossi2010@gmail.com>
2021-05-21 09:42:20 -0300

    Build (home page component): mobile icon

---
+++ frontend/src/components/pagesComponents/burguerIcon.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mobile_menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .mobile_menu{
+        /* display: none; */
+        cursor: pointer;
+        transition: 0.3s;
+    }
+
+    .mobile_menu:hover{
+        opacity: 0.7;
+    }
+
+    .mobile_menu div{
+        width: 32px;
+        height: 2px;
+        background: #ffff;
+        margin: 8px;
+    }
+</style>
